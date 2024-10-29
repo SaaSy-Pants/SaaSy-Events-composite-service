60c14da6219253564e12337c898db7eb513f0f43
Nirav Agarwal <59258743+Nirav-Agarwal@users.noreply.github.com>
2024-10-29 17:25:39 -0400

    fixed port

--- app/utils/config.py
+++ app/utils/config.py
@@ -7,9 +7,9 @@ class Config:
     #Microservices URLs
     USER_MGMT_URL: str = os.getenv("USER_MGMT_URL", "http://localhost:8000")
     EVENT_MGMT_URL: str = os.getenv("EVENT_MGMT_URL", "http://localhost:8001")
-    TICKET_URL: str = os.getenv("TICKET_URL", "http://localhost:8001")
+    TICKET_URL: str = os.getenv("TICKET_URL", "http://localhost:8002")
 
     #Composite Service Configurations
-    COMPOSITE_SERVICE_PORT: int = int(os.getenv("COMPOSITE_SERVICE_PORT", 8002))
+    COMPOSITE_SERVICE_PORT: int = int(os.getenv("COMPOSITE_SERVICE_PORT", 8003))
 
 
