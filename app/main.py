f462f037aa004208b6f9e2fe654e31a780d7fcce
Nirav Agarwal <59258743+Nirav-Agarwal@users.noreply.github.com>
2024-10-29 13:08:20 -0400

    added uvicorn.run

--- app/main.py
+++ app/main.py
@@ -30,3 +30,6 @@ app.include_router(health.router)
 @app.get("/", tags=["root"])
 async def read_root():
     return {"message": "Welcome to the Composite Service!"}
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8002, reload=True)
